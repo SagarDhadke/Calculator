a020f9a3480313d6d5bdc43c87f46f3c292e5c5b
Sagar Dhadke <peaceunique2003@gmail.com>
2024-08-06 12:19:39 +0530

    Complete 2 Rows

--- lib/main.dart
+++ lib/main.dart
@@ -11,82 +11,189 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: 
+      home: MyCalculator(),
     );
   }
 }
 
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyCalculator extends StatefulWidget {
+  const MyCalculator({super.key});
 
   @override
+  State<MyCalculator> createState() => _MyCalculatorState();
+}
+
+class _MyCalculatorState extends State<MyCalculator> {
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          "Calculator"),
+        backgroundColor: Colors.amber,  
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
+      body: SizedBox(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                    height: 75,
+                    width: 75,
+
+                    child: Text("AC", style: TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold, 
+                      color: Colors.white,
+                    ),),
+                    
+                ),
+
+              SizedBox(width: 10,),
+
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.grey
+                ),
+                height: 75,
+                width: 75,
+
+              child: Text("+/-", style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white
+              ),),
+
+              ),
+
+              SizedBox(width: 10),
+
+              Container(
+                alignment: Alignment.center,
+                width: 75,
+                height: 75,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+
+                child: Text("%", style: TextStyle(fontSize: 25,
+                 fontWeight: FontWeight.bold,
+                 color: Colors.white),),
+              ),
+
+              SizedBox(width: 10,),
+
+              Container(
+                alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.red,
+                  ),
+                  width: 75,
+                  height: 75,
+              
+              child: Text("/", style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white
+              ),),
+              
+              ),
+
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            SizedBox(height: 20),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.blueGrey,
+                ),
+                alignment: Alignment.center,
+                height: 75,
+                width: 75,
+
+              child: Text("7", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,
+              color: Colors.white),),
+
+              ),
+
+              SizedBox(width: 10),
+
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.blueGrey
+                ),
+                height: 75,
+                width: 75,
+
+              child: Text("8", style: TextStyle(fontSize: 25, color: Colors.white)),
+
+              ),
+
+              SizedBox(width: 10,),
+
+              Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.blueGrey),
+
+                    width: 75,
+                    height: 75,
+
+                child: Text("9", style: TextStyle(
+                  fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white
+                ),),
+
+              ),
+
+              SizedBox(
+                  width: 10,
+              ),
+
+            Container(
+              alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.red
+                ),
+                height: 75,
+                width: 75,
+
+              child: Text("X", style: TextStyle(fontSize: 25,
+               fontWeight: FontWeight.bold, color: Colors.white),),
+
             ),
+
+            ],
+          )
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
+  
